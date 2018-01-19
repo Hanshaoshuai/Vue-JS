4b32f99b808f3b4644de5fbf4470f1722d18bbcb
Hanshaoshuai <361062939@qq.com>
2018-01-19 09:59:50 +0800

    tianjia

---
+++ huaqiangkeji-anZhuanBao/path.js
@@ -0,0 +1,45 @@
+//
+//
+//export function path(){
+//	var URL = {
+//		path: 'http://www.qironghome.com/api/index.php', 
+//		path2: 'http://www.qironghome.com/index.php', //不用授权的接口地址
+//		path3: 'http://www.qironghome.com/index.php/app/' //h5分享页面的地址
+//	
+//	// 	path: 'http://47.93.63.26/zbq/web/api/index.php', //接口地址
+//	// 	path2: 'http://47.93.63.26/zbq/web/index.php', //不用授权的接口地址
+//	// 	path3: 'http://47.93.63.26/zbq/web/index.php/app/' //h5分享页面的地址	
+//	   	
+//	// 	path: 'http://test.qironghome.com/zbq/web/api/index.php', //接口地址
+//	// 	path2: 'http://test.qironghome.com/zbq/web/index.php', //不用授权的接口地址
+//	// 	path3: 'http://test.qironghome.com/zbq/web/index.php/app/' //h5分享页面的地址	2
+//	   	
+//	// 	path: 'http://192.168.16.100/zbq/web/api/index.php', //接口地址
+//	//  path2: 'http://192.168.16.100/zbq/web/index.php', //不用授权的接口地址
+//	//  path3: 'http://192.168.16.100/zbq/web/index.php/app/' //h5分享页面的地址
+//	}
+//	return URL;
+//}
+var URL = {
+	path: 'http://www.qironghome.com/api/index.php/v2/', 
+	path1: 'http://www.qironghome.com/api/index.php/', //不用授权的接口地址
+
+// 	path: 'http://test.qironghome.com/api/index.php/v2/', //接口地址test.qironghome.com/bak/web/api/index.php/upload/photo
+// 	path1: 'http://test.qironghome.com/api/index.php/', //接口地址
+
+
+
+// 	path2: 'http://192.168.16.100/bak/web/api/index.php/', //不用授权的接口地址  本机
+// 	path3: 'https://47.93.63.26/zbq/web/index.php/app/' //h5分享页面的地址	
+   	
+// 	path: 'http://test.qironghome.com/zbq/web/api/index.php', //接口地址
+// 	path2: 'http://test.qironghome.com/zbq/web/index.php', //不用授权的接口地址
+// 	path3: 'http://test.qironghome.com/zbq/web/index.php/app/' //h5分享页面的地址	2
+   	
+// 	path: 'http://192.168.16.100/zbq/web/api/index.php', //接口地址
+//  path2: 'http://192.168.16.100/zbq/web/index.php', //不用授权的接口地址
+//  path3: 'http://192.168.16.100/zbq/web/index.php/app/' //h5分享页面的地址
+}
+
+
+
